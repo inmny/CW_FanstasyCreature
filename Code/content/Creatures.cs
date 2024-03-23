e048102d02431b7bb6f6a58a625b70e054c3dcfc
Moouqi <moqi12306@gmail.com>
2024-03-23 12:23:47 +0800

    初步添加奇幻生物

--- Code/content/Creatures.cs
+++ Code/content/Creatures.cs
@@ -53,6 +53,271 @@ internal class Creatures : ExtendedLibrary<CW_ActorAsset>
         CreateButton("half_deer_man");
         #endregion
 
+        #region ------半人马
+        // 创建一个生物半人马
+        CreateActor("centaur","Centaur","iconCentaur",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("Centaur_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("centaur");
+        #endregion
+        
+        #region ------阿努比斯
+        // 创建一个生物阿努比斯
+        CreateActor("anubis","Anubis","iconAnubis",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("anubis");
+        #endregion
+                  
+        #region ------骨龙
+        // 创建一个生物骨龙
+        CreateActor("ossaurus","Ossaurus","iconOssaurus",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("ossaurus");
+        #endregion
+                                        
+        #region ------法老
+        // 创建一个生物法老
+        CreateActor("pharaoh","Pharaoh","iconPharaoh",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("pharaoh");
+        #endregion
+                                                
+        #region ------骷髅骑士
+        // 创建一个生物骷髅骑士
+        CreateActor("skeleton_knight","Skeleton Knight","iconSkeleton_Knight",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("skeleton_knight");
+        #endregion
+                                                                
+        #region ------斯芬克斯
+        // 创建一个生物斯芬克斯
+        CreateActor("sphinx","Sphinx","iconSphinx",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("sphinx");
+        #endregion
+
+        #region ------吸血鬼
+        // 创建一个生物吸血鬼
+        CreateActor("bloodsucker","Bloodsucker","iconBloodsucker",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("bloodsucker");
+        #endregion
+
+        #region ------吸血鬼猎人
+        // 创建一个生物吸血鬼猎人
+        CreateActor("vampire_hunter","Vampire Hunter","iconVampire_Hunter",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("vampire_hunter");
+        #endregion
+
+        #region ------狼人
+        // 创建一个生物狼人
+        CreateActor("werewolf","Werewolf","iconWerewolf",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("werewolf");
+        #endregion
+
+        #region ------朱雀
+        // 创建一个生物朱雀
+        CreateActor("suzaku","Suzaku","iconSuzaku",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("suzaku");
+        #endregion
+
+        #region ------麒麟
+        // 创建一个生物麒麟
+        CreateActor("qilin","Qilin","iconQilin",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("qilin");
+        #endregion
+
+        #region ------白虎
+        // 创建一个生物白虎
+        CreateActor("white_tiger","White Tiger","iconWhite_Tiger",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = false; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("white_tiger");
+        #endregion
+                                                                                                        
+        #region ------玄武
+        // 创建一个生物玄武
+        CreateActor("xuanwu","Xuanwu","iconXuanwu",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("xuanwu");
+        #endregion
+
+        #region ------中国龙
+        // 创建一个生物中国龙
+        CreateActor("loong","Loong","iconLoong",out vanilla_t,out stats);
+        t.prefer_element = new[] { 25, 20, 10, 15, 30 }; // 倾向于地灵根
+        t.prefer_element_scale = 0.9f; // 倾向程度
+        t.add_allowed_cultisys("cw_cultisys_immortal"); // 允许修仙
+        // 武道: cw_cultisys_bushido
+        // 魂道: cw_cultisys_soul
+        vanilla_t.disableJumpAnimation = true; //允许跳跃
+        vanilla_t.needFood = false; // 不需要食物
+        // 其他原版设置见 https://github.com/inmny/Cultivation-Way-Core/blob/base_14/Code/W_Content_Actor.cs 第200行开始，
+        // 作用于0.14游戏版本，大体没有发生改变
+        stats[S.damage] = 1000; // 伤害
+        stats[CW_S.wakan] = 100; // 灵气
+        MarkNameTemplate("deer_name"); // 设置命名模板，只在中文名存在时生效
+        // 创建这个生物的放置按钮
+        CreateButton("loong");
+        #endregion
     }
 
     /// <summary>
